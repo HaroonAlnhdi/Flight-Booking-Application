5ae66daff966f9374f6dbb7d2aea0bed322f6550
ali-alaalem <Meemo2002a@gmail.com>
2024-08-17 19:38:17 +0300

    adding the ability for the user to Edit his profile or delete it

--- controllers/users.js
+++ controllers/users.js
@@ -1,26 +1,39 @@
-const express = require('express');
+const express = require("express");
 // auth
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Models
-const User = require('../models/user');
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
-    const { username, password ,first_name,last_name,email,phone_number} = req.body;
+    const { username, password, first_name, last_name, email, phone_number } =
+      req.body;
 
     const existingUser = await User.findOne({ username });
 
     if (existingUser) {
-      return res.status(400).json({ error: 'Something went wrongm, try again.' });
+      return res
+        .status(400)
+        .json({ error: "Something went wrongm, try again." });
     }
 
-    const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
+    const hashedPassword = bcrypt.hashSync(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
 
-    const user = await User.create({ username, hashedPassword ,first_name,last_name,email,phone_number});
+    const user = await User.create({
+      username,
+      hashedPassword,
+      first_name,
+      last_name,
+      email,
+      phone_number,
+    });
 
     const token = jwt.sign(
       {
@@ -32,24 +45,27 @@ router.post('/signup', async (req, res) => {
 
     return res.status(201).json({ user, token });
   } catch (error) {
-    res.status(400).json({ error: 'Something wen wrong, try again.' });
+    res.status(400).json({ error: "Something wen wrong, try again." });
   }
 });
 
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     const existingUser = await User.findOne({ username });
 
     if (!existingUser) {
-      return res.status(400).json({ error: 'Invalid Credentials' });
+      return res.status(400).json({ error: "Invalid Credentials" });
     }
 
-    const isValidPassword = bcrypt.compareSync(password, existingUser.hashedPassword);
+    const isValidPassword = bcrypt.compareSync(
+      password,
+      existingUser.hashedPassword
+    );
 
     if (!isValidPassword) {
-      throw Error('Invalid Credentials');
+      throw Error("Invalid Credentials");
     }
 
     const token = jwt.sign(
@@ -62,9 +78,33 @@ router.post('/signin', async (req, res) => {
 
     return res.status(200).json({ user: existingUser, token });
   } catch (error) {
-    res.status(400).json({ error: 'Something went wrong, try again.' });
+    res.status(400).json({ error: "Something went wrong, try again." });
   }
 });
 
-module.exports = router;
+router.put("/:userId", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true }
+    );
 
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:userId", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+module.exports = router;
