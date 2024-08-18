15b9bc9050cf62dde090bd2bf81f22c498b938f5
ali-alaalem <Meemo2002a@gmail.com>
2024-08-18 20:05:54 +0300

    Moving the user Delete and Edit from the user controller to the profilr controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -16,4 +16,29 @@ router.get("/:userId", async (req, res) => {
     res.status(500).json(error);
   }
 });
+router.put("/:userId", async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:userId", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.userId);
+
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -82,37 +82,6 @@ router.post("/signin", async (req, res) => {
   }
 });
 
-router.put("/:userId", async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.userId,
-      req.body,
-      { new: true }
-    );
 
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-});
 
-router.delete("/:userId", async (req, res) => {
-  try {
-    const deletedUser = await User.findByIdAndDelete(req.params.userId);
-
-    res.status(200).json(deletedUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
-});
-router.get("profile/:userId", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
 module.exports = router;
