d2be6742cd6c47e749ba34fc425725aae0275ee2
ali-alaalem <Meemo2002a@gmail.com>
2024-08-20 16:06:10 +0300

    fixing the add ticket problem by Editing the booking middeleware by assigning the tickets to number

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,36 +1,35 @@
-const express  = require('express');
-const router = express.Router({mergeParams: true});
+const express = require("express");
+const router = express.Router({ mergeParams: true });
 
-const User = require('../models/user');
-const Trip = require('../models/Trip');
+const User = require("../models/user");
+const Trip = require("../models/Trip");
 
-router.post('/:tripId', async (req, res, next) => {
-    try {
-        const user = await  User.findById(req.user._id);
-        const tripInfo = await Trip.findById(req.params.tripId);
-        if (!tripInfo) {
-            throw new Error('Something went wrong');
-        } 
-        if (!user) {
-            throw new Error('Something went wrong');
-        } 
-        const booking = {
-            tickets: req.body,
-            depTripDate: tripInfo.dep_date_time,
-            arrTripDate: tripInfo.arr_date_time,
-            price: tripInfo.price,
-            trip: tripInfo._id
-        };
-        tripInfo.tickets = tripInfo.tickets - 1;
-        user.bookings.push(booking);
-        await user.save();
-        await tripInfo.save();
-        res.status(201).json(user);
-    } catch (error) {
-        console.error('Error details:', error);
-        res.status(500).json({ error: error.message });
+router.post("/:tripId", async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const tripInfo = await Trip.findById(req.params.tripId);
+    if (!tripInfo) {
+      throw new Error("Something went wrong");
     }
+    if (!user) {
+      throw new Error("Something went wrong");
+    }
+    const booking = {
+      tickets: 1, // Number of tickets TODO: how to add more?
+      depTripDate: tripInfo.dep_date_time,
+      arrTripDate: tripInfo.arr_date_time,
+      price: tripInfo.price,
+      trip: tripInfo._id,
+    };
+    tripInfo.tickets = tripInfo.tickets - 1;
+    user.bookings.push(booking);
+    await user.save();
+    await tripInfo.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error details:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
-
 
 module.exports = router;
